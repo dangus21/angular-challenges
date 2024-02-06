1a15aec08cb925a9f87e01c16d26a1f9618af47e
Miguel Magalhães <miguel.magalhaes@criticalsoftware.com>
2024-02-06 13:10:54 +0000

    Projection

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+	FakeHttpService,
+	randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
-  selector: 'app-city-card',
-  template: 'TODO City',
-  standalone: true,
-  imports: [],
+	selector: 'app-city-card',
+	template: `
+		<app-card
+			[list]="cities$ | async"
+			(add)="addStudent()"
+			class="bg-light-green">
+			<img src="assets/img/city.png" width="200px" />
+			<ng-template #rowRef let-city>
+				<app-list-item (delete)="deleteStudent(city.id)">
+					{{ city.country }}
+				</app-list-item>
+			</ng-template>
+		</app-card>
+	`,
+	standalone: true,
+	styles: [
+		`
+			.bg-light-green {
+				background-color: rgba(100, 100, 250, 0.1);
+			}
+		`,
+	],
+	imports: [CommonModule, CardComponent, ListItemComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+	constructor(
+		private http: FakeHttpService,
+		private store: CityStore,
+	) {}
 
-  ngOnInit(): void {}
+	cities$ = this.store.cities$;
+
+	ngOnInit(): void {
+		this.http.fetchCities$.subscribe((s) => this.store.addAll(s));
+	}
+
+	addStudent() {
+		this.store.addOne(randomCity());
+	}
+
+	deleteStudent(id: number) {
+		this.store.deleteOne(id);
+	}
 }
--- apps/angular/projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/projection/src/app/component/student-card/student-card.component.ts
@@ -1,40 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+	FakeHttpService,
+	randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
-import { CardType } from '../../model/card.model';
-import { Student } from '../../model/student.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
-  selector: 'app-student-card',
-  template: `
-    <app-card
-      [list]="students"
-      [type]="cardType"
-      customClass="bg-light-green"></app-card>
-  `,
-  standalone: true,
-  styles: [
-    `
-      ::ng-deep .bg-light-green {
-        background-color: rgba(0, 250, 0, 0.1);
-      }
-    `,
-  ],
-  imports: [CardComponent],
+	selector: 'app-student-card',
+	template: `
+		<app-card
+			[list]="students$ | async"
+			(add)="addStudent()"
+			class="bg-light-green">
+			<img src="assets/img/student.webp" width="200px" />
+			<ng-template #rowRef let-student>
+				<app-list-item (delete)="deleteStudent(student.id)">
+					{{ student.firstName }}
+				</app-list-item>
+			</ng-template>
+		</app-card>
+	`,
+	standalone: true,
+	styles: [
+		`
+			.bg-light-green {
+				background-color: rgba(0, 250, 0, 0.1);
+			}
+		`,
+	],
+	imports: [CommonModule, CardComponent, ListItemComponent],
 })
 export class StudentCardComponent implements OnInit {
-  students: Student[] = [];
-  cardType = CardType.STUDENT;
+	constructor(
+		private http: FakeHttpService,
+		private store: StudentStore,
+	) {}
 
-  constructor(
-    private http: FakeHttpService,
-    private store: StudentStore,
-  ) {}
+	students$ = this.store.students$;
 
-  ngOnInit(): void {
-    this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+	ngOnInit(): void {
+		this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+	}
 
-    this.store.students$.subscribe((s) => (this.students = s));
-  }
+	addStudent() {
+		this.store.addOne(randStudent());
+	}
+
+	deleteStudent(id: number) {
+		this.store.deleteOne(id);
+	}
 }
